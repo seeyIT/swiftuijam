3de733c010a444223221c9782d96f980a70cd4b4
Kornel Miszczak <k.miszczak@2040.io>
2021-02-19 22:10:14 +0100

    add git add index.js  test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 const port = process.env.PORT || 3000
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello World!!!!');
 });
 
 app.post('/', function(req, res) {
