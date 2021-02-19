b0b555a0b66855244f45e8418e906e516153bd45
Kornel Miszczak <k.miszczak@2040.io>
2021-02-19 22:09:24 +0100

    env

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const http = require('http');
+var express = require('express');
+var app = express();
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+app.post('/', function(req, res) {
+  res.send('Hello World!');
 });
+
+app.listen(port, function() {
+  console.log('Example app listening on port 3000!');
+});
+
+// const http = require('http');
+// const port = process.env.PORT || 3000
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
+
+// server.listen(port,() => {
+//   console.log(`Server running at port `+port);
+// });
