9f152e3dbd151a26bffbc631429f2512ae1a353f
Kornel Miszczak <k.miszczak@2040.io>
2021-02-19 22:07:08 +0100

    change

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-var express = require('express');
-var app = express();
+const http = require('http');
+const port = process.env.PORT || 3000
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
 });
 
-app.post('/', function(req, res) {
-  res.send('Hello World!');
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
 });
-
-app.listen(5908, function() {
-  console.log('Example app listening on port 3000!');
-});
-
