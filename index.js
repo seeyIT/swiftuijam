66565106d0cc9e42832903d09594c6320a54d434
Kornel Miszczak <k.miszczak@2040.io>
2021-02-20 19:46:22 +0100

    add comments and option to clear all appointments

--- index.js
+++ index.js
@@ -2,28 +2,33 @@ const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const csv = require('csv-parser')
-var uuid = require('uuid');
+const uuid = require('uuid');
 
 const app = express();
 const port = process.env.PORT || 3000
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://swiftuijamlogin:PublicPassowrd@cluster0.a5xor.mongodb.net/swiftuijam?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', function(req, res) {
-  res.send('Hello World!!!!' + uuid.v4());
-
+	res.send('Welcome! It is the best SwiftUI Jam API :)');
 });
 
-app.post('/', function(req, res) {
-  res.send('Hello World!');
-});
+/*
+*
+* Part related to handle registration to be vaccinated
+*
+*/
 
-
-app.get('/slots/:hospital', function(req, res) {
+// Endpoint returns all slots for needed hospital
+// Params:
+// - hospitalName: Stirng
+app.get('/slots/:hospitalName', function(req, res) {
+	if (req.params.hospitalName == null) {
+		res.status(400).json({"message":"hispital name is missing"});
+		return;
+	}
 	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-	console.log("Hospital: " + req.params.hospital);
 	client.connect(err => {
 		const db = client.db("swiftuijam");
 		const collection = db.collection("hospital_slots");
@@ -38,16 +43,20 @@ app.get('/slots/:hospital', function(req, res) {
 	});
 });
 
-
-app.get('/appointments/:id', function(req, res) {
+// Endpoint returns appointments for user ID
+// Params:
+// - userId: Stirng
+app.get('/appointments/:userId', function(req, res) {
+	if (req.params.userId == null) {
+		res.status(400).json({"message":"user id is missing"});
+		return;
+	}
 	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-	console.log("Hospital: " + req.params.hospital);
 	client.connect(err => {
 		const db = client.db("swiftuijam");
 		const collection = db.collection("appointment");
-		const data = collection.find({"user_id":req.params.id}).toArray(function(err, result) {
+		const data = collection.find({"user_id":req.params.userId}).toArray(function(err, result) {
 	   		if (err) throw err;
-	    	console.log(result);
 	    	client.close();
 	    	res.json(result);
 		});
@@ -56,13 +65,20 @@ app.get('/appointments/:id', function(req, res) {
 	});
 });
 
+// Endpoint books appointemnt and returns its uuid if slot is free
+// Params needed:
+// - userId: Stirng
+// - hospitalName: String
+// - timeSlot: String
 app.post('/bookAppointment', function(req, res) {
 	const userId = req.params.userId;
 	const hospitalName = req.params.hospitalName;
 	const timeSlot = req.params.timeSlot;
-	console.log("userId: " + userId);
-	console.log("hospitalName: " + hospitalName);
-	console.log("timeSlot: " + timeSlot);
+	if (userId == null || hospitalName == null || timeSlot == null) {
+		res.status(400).json({"status":"error","message":"no enought parameters"});
+		return;
+	}
+
 	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 	client.connect(err => {
@@ -70,10 +86,7 @@ app.post('/bookAppointment', function(req, res) {
 		const collection = db.collection("hospital_slots");
 		const data = collection.find({"hospital_name":hospitalName}).toArray(function(err, result) {
 	   		if (err) throw err;
-	    	console.log(result);
-
 	    	const slot = result[0]["slots"][timeSlot];
-	    	console.log("slot: " + slot);
 	    	if (slot !== "free") {
 	    		res.status(400).json({"status":"error"});
 	    		client.close();
@@ -87,80 +100,22 @@ app.post('/bookAppointment', function(req, res) {
 	    		"hospita_name": hospitalName,
 	    		"time_slot": timeSlot
 	    	}
-	    	var slots = result[0]["slots"];
+	    	const slots = result[0]["slots"];
 	    	slots[timeSlot] = newUuid;
-	    	console.table(slots);
-	    	collection.updateOne({"hospital_name":hospitalName}, {$set: { slots }}, function(err1, res1) {
-			    if (err1) throw err1;
-			    console.log("1 document updated");
+
+	    	collection.updateOne({"hospital_name":hospitalName}, {$set: { slots }}, function(hospitalError, hospitalResult) {
+			    if (hospitalError) throw err1;
 
 			    const appointmentCollection = db.collection("appointment");
-	    		appointmentCollection.insertOne(appointment, function(err2, res2) {
-	    			console.log("res 2: " + res2);
-	    			console.log("err2: " + err2);
-
+	    		appointmentCollection.insertOne(appointment, function(appointmentError, appointmentResult) {
 			    	res.status(200).json({"status":"ok","uuid":newUuid});
-
 	    		});
-		  	});
-
-	    	
-	    	
-
-	    		
+		  	});	
+		});
 	});
 });
 
-});
-
-app.get('/bookAppointment/:userId/:hospitalName/:timeSlot', function(req, res) {
-	const userId = req.params.userId;
-	const hospitalName = req.params.hospitalName;
-	const timeSlot = req.params.timeSlot;
-	console.log("userId: " + userId);
-	console.log("hospitalName: " + hospitalName);
-	console.log("timeSlot: " + timeSlot);
-	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-	client.connect(err => {
-		const db = client.db("swiftuijam");
-		const collection = db.collection("hospital_slots");
-		const data = collection.find({"hospital_name":hospitalName}).toArray(function(err, result) {
-	   		if (err) throw err;
-	    	console.log("result: " + result);
-
-
-	    	const slot = result[0]["slots"][timeSlot];
-	    	console.log("slot: " + slot);
-	    	if (slot !== "free") {
-	    		res.status(400).json({"status":"error"});
-	    		client.close();
-	    		return;
-	    	}
-
-	    	const newUuid = uuid.v4();
-	    	const appointment = {
-	    		"uuid": newUuid,
-	    		"user_id": userId,
-	    		"hospita_name": hospitalName,
-	    		"time_slot": timeSlot
-	    	}
-	    	var slots = result[0]["slots"];
-	    	slots[timeSlot] = newUuid;
-	    	console.table(slots);
-	    	collection.updateOne({"hospital_name":hospitalName}, {$set: { slots }});
-
-	    	const appointmentCollection = db.collection("appointment");
-	    	appointmentCollection.insertOne(appointment);
-	    	
-
-	    	res.status(200).json({"status":"ok","uuid":newUuid});
-	    		
-	});
-});
-
-});
-
+// Util endpoint to clear database records
 app.get('/clearSlotsData', function(req, res) {
 	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 	client.connect(err => {
@@ -180,22 +135,45 @@ app.get('/clearSlotsData', function(req, res) {
 	    		
 	    	}
 	    	collection.updateOne({"hospital_name":"Hospital1"}, {$set: { slots }});
-	    	res.status(200).json({"status":"ok"});
-	    		
+	    	res.status(200).json({"status":"ok"});	
+		});
 	});
 });
 
+app.get('/clearAppointmentsData', function(req, res) {
+	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+	client.connect(err => {
+		const db = client.db("swiftuijam");
+		const collection = db.collection("appointment");
+		const data = collection.deleteMany({}, function(err, result) {
+	   		if (err) throw err;
+	    
+
+	    	res.status(200).json({"status":"ok"});	
+		});
+	});
 });
 
+/*
+*
+* Part related to fetch data related to numbers of vaccinations in different countries
+*
+*/
 
 
-
-app.get('/newestData/:country', function(req, res) {
+//Endpoint returns the newest statistics for the country selected in request parameters
+// Params needed:
+// - countryName: Stirng
+app.get('/newestData/:countryName', function(req, res) {
+	if (req.params.countryName == null) {
+		res.status(400).json({"message":"user id is missing"});
+		return;
+	}
 	const results = [];
-	const file = fs.createWriteStream("data/"+req.params.country+".csv");
-		https.get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/"+req.params.country+".csv", function(response) {
+	const file = fs.createWriteStream("data/"+req.params.countryName+".csv");
+		https.get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/"+req.params.countryName+".csv", function(response) {
 		response.pipe(file);
-		fs.createReadStream("data/" + req.params.country+".csv")
+		fs.createReadStream("data/" + req.params.countryName+".csv")
 			.pipe(csv())
 			.on('data', (data) => results.push(data))
 			.on('end', () => {
@@ -204,12 +182,19 @@ app.get('/newestData/:country', function(req, res) {
 	});
 });
 
-app.get('/allData/:country', function(req, res) {
+//Endpoint returns the all statistics for some country selected in request parameters
+// Params needed:
+// - countryName: Stirng
+app.get('/allData/:countryName', function(req, res) {
+	if (req.params.countryName == null) {
+		res.status(400).json({"message":"countryName name is missing"});
+		return;
+	}
 	const results = [];
-	const file = fs.createWriteStream("data/"+req.params.country+".csv");
-	https.get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/"+req.params.country+".csv", function(response) {
+	const file = fs.createWriteStream("data/"+req.params.countryName+".csv");
+	https.get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/"+req.params.countryName+".csv", function(response) {
 		response.pipe(file);
-		fs.createReadStream("data/" + req.params.country+".csv")
+		fs.createReadStream("data/" + req.params.countryName+".csv")
 			.pipe(csv())
 			.on('data', (data) => results.push(data))
 			.on('end', () => {
@@ -218,26 +203,10 @@ app.get('/allData/:country', function(req, res) {
 	});
 });
 
-app.get('/countrydata/:country', function(req, res) {
-	const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-	console.log(req.params);
-
-	client.connect(err => {
-		const db = client.db("swiftuijam");
-		const collection = db.collection("country_data");
-		const data = collection.find({"location": req.params.country}).toArray(function(err, result) {
-	   		if (err) throw err;
-	    	console.log(result);
-	    	client.close();
-	    	res.json(result);
-		});
-
-	})
-});
 
 
 app.listen(port, function() {
-  console.log('Example app listening on port 3000!');
+  console.log('App started');
 });
 
 
