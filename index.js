0ffe4fd1845ab078617bff7b524b9298f9da90f6
Kornel Miszczak <k.miszczak@2040.io>
2021-02-20 11:10:28 +0100

    add request to mongo

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://swiftuijamlogin:PublicPassowrd@cluster0.a5xor.mongodb.net/swiftuijam?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', function(req, res) {
   res.send('Hello World!!!!');
@@ -10,19 +14,22 @@ app.post('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/countrydata', function(req, res) {
+	client.connect(err => {
+		const db = client.db("swiftuijam");
+		const collection = db.collection("country_data");
+		const data = collection.find({}).toArray(function(err, result) {
+	   		if (err) throw err;
+	    	console.log(result);
+	    	client.close();
+	    	res.json(result);
+		});
+
+	})
+});
+
 app.listen(port, function() {
   console.log('Example app listening on port 3000!');
 });
 
-// const http = require('http');
-// const port = process.env.PORT || 3000
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/html');
-//   res.end('<h1>Hello World</h1>');
-// });
-
-// server.listen(port,() => {
-//   console.log(`Server running at port `+port);
-// });
