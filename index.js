116ab19531cf47de1b98277473e60e5206ebb89d
Kornel Miszczak <k.miszczak@2040.io>
2021-03-05 22:35:02 +0100

    add cors, allow all

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const fs = require('fs');
 const csv = require('csv-parser')
 const uuid = require('uuid');
 
-const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 const port = process.env.PORT || 3000
 
 const MongoClient = require('mongodb').MongoClient;
